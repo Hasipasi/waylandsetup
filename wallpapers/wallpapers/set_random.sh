e0c12a0ffcd5135ab6fe00d3d79b4df9ee779611
bodis <bodis@localhost>
2026-03-17 15:19:29 +0100

    Use center-crop scaling for wallpapers on any aspect ratio

--- wallpapers/wallpapers/set_random.sh
+++ wallpapers/wallpapers/set_random.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 WALLPAPER_DIR="$HOME/wallpapers"
-WALLPAPERS=("$WALLPAPER_DIR"/*)
+WALLPAPERS=()
+for f in "$WALLPAPER_DIR"/*.{jpg,jpeg,png,webp}; do
+    [ -f "$f" ] && WALLPAPERS+=("$f")
+done
 RANDOM_WALL=${WALLPAPERS[$RANDOM % ${#WALLPAPERS[@]}]}
-swww img "$RANDOM_WALL"
+swww img "$RANDOM_WALL" --scaling Crop
